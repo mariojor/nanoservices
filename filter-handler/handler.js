b7197c39e4ccebfe1ae7c66afd5521bfc9d66b52
mariojor <mariojorge.sdei@gmail.com>
2021-12-05 18:30:18 -0300

    filter-handler

---
+++ filter-handler/handler.js
@@ -0,0 +1,9 @@
+'use strict';
+const filterService = require('./service/filterService')
+
+module.exports.filter = async (event) => {
+  console.log('Evento do SNS recebido com sucesso:', JSON.stringify(event))
+  await filterService.filter(event)
+
+  return { message: 'Filter executed successfully!', event };
+};
