9138eaeb4ae6abbf88fcb1bdde86110254bed706
mariojor <mariojorge.sdei@gmail.com>
2021-12-05 02:28:53 -0300

    SNS disparado

--- thumbnail-handler/handler.js
+++ thumbnail-handler/handler.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports.thumbnail = async (event) => {
+console.log('Evento do SNS recebido com sucesso:', JSON.stringify(event))
   return {
     statusCode: 200,
     body: JSON.stringify(
