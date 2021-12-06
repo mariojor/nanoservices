0c94ee9d8030a00878bb77bcf0047c6454c15d51
mariojor <mariojorge.sdei@gmail.com>
2021-12-06 08:10:11 -0300

    dev

--- image-tagging-filter/handler.js
+++ image-tagging-filter/handler.js
@@ -1,18 +1,35 @@
 'use strict';
+const AWS = require("aws-sdk")
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+AWS.config.update({
+  region: 'us-east-1'
+})
+
+const rekognition = new AWS.Rekognition()
+
+module.exports.tag = async (event) => {
+  const s3Info = JSON.parse(event.Records[0].Sns.Message)
+  const bucket = s3Info.Records[0].s3.bucket.name
+  const key = s3Info.Records[0].s3.object.key
+
+  const data = await (new Promise((res, rej) => {
+    rekognition.detectLabels({
+      Image: {
+        S3Object: {
+          Bucket: bucket,
+          Name: key
+        }
       },
-      null,
-      2
-    ),
-  };
+      MinConfidence: 80,
+      MaxLabels: 6
+    }, (err, data) => {
+      if (err) {
+        return (rej(err))
+      }
+      return res(data)
+    })
+  }));
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  console.log(data.Labels)
+  return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
